12cb8b394a1fcba4821fd950506eddde6e2660f1
Hikaru77ko <kusunosehikari@kusunosehikarinoMacBook-Pro.local>
2022-03-23 21:38:53 +0900

    add_content

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(function () {
+    let $win = $(window),
+        $fv = $(".fv"),
+        $header = $(".header"),
+        fvHeight = $fv.outerHeight();
+    fixedClass = "fixed";
+
+    $win.on("load scroll", function () {
+        let value = $(this).scrollTop();
+        if ($win.width() > 768) {
+            if (value > fvHeight) {
+                $header.addClass(fixedClass);
+            } else {
+                $header.removeClass(fixedClass);
+            }
+        }
+    });
+});
+
+$(".slider").slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    speed: 1000,
+    cssEase: "linear",
+});
+
+$(".burger-btn").on("click", function () {
+    $(".header-nav").fadeToggle(300);
+    $(this).toggleClass("cross");
+    $('body').toggleClass('noscroll')
+});
